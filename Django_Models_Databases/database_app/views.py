e223fabd91f5cd58d9d5e80cc2e637f627870320
Taner Süzen <taner.suzen95@gmail.com>
2024-02-12 22:54:01 +0300

    commit before tweet app

--- Django_Models_Databases/database_app/views.py
+++ Django_Models_Databases/database_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+
+def index(request):
+    musician_data = models.Musician.objects.all()
+    musician_context = {"musicians" : musician_data}
+    return render(request, 'database_app/index.html', context=musician_context)
